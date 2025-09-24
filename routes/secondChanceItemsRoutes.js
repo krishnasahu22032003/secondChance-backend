dede76f44fc5f6866bf0c9b1ef65114d11b2bb2e
krishna sahu <krishna.sahu.work@gmail.com>
2025-09-24 23:27:56 +0530

    new

--- routes/secondChanceItemsRoutes.js
+++ routes/secondChanceItemsRoutes.js
@@ -7,3 +7,5 @@ router.get("/api/secondchance/items", async (req, res) => {
   const items = await db.collection("secondChanceItems").find().toArray();
   res.json(items);
 });
+router.get("/api/secondchance/items", async (req, res) => { ... });
+router.get("/api/secondchance/items/:id", async (req, res) => { ... });
