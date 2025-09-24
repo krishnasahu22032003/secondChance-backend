2a92b36b8cd7ee3e55479ffed9490beb644ab2d8
krishna sahu <krishna.sahu.work@gmail.com>
2025-09-24 23:26:36 +0530

    Update

---
+++ routes/secondChanceItemsRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const { connectToDatabase } = require("../models/db");
+const router = express.Router();
+
+router.get("/api/secondchance/items", async (req, res) => {
+  const db = await connectToDatabase();
+  const items = await db.collection("secondChanceItems").find().toArray();
+  res.json(items);
+});
